0294e3b97a9ac70803fd17587f98a165fd0c861c
198144 <198144@CARBON.CTI.P.LODZ.PL>
2018-04-15 11:02:21 +0200

    fix server random api return

--- server/api/routes/mainRoutes.js
+++ server/api/routes/mainRoutes.js
@@ -14,6 +14,6 @@ function randomApi (req, res) {
     ];
 
     res.status(200);
-    //res.json(apis[Math.floor(Math.random()*apis.length)]);
-    res.json(apis[0]);
+    res.json(apis[Math.floor(Math.random()*apis.length)]);
+    //res.json(apis[0]);
 }
